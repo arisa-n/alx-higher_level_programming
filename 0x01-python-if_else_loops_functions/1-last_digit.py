3d594eb96294a227482ebbc3f4a361e387ec6420
arisa-e <emmanuelarisa@outlook.com>
2022-08-24 00:45:33 +0300

    initial commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,12 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     num = number * -1
-    num= num % 10
+    num = num % 10
 else:
-    num= number % 10
-if num >5:
+    num = number % 10
+if num > 5:
     print("last digit of {} is {} and is greater than 5" .format(number, num))
-elif num <6 and num != 0:
+elif num < 6 and num != 0:
     print("last digit of {} is {} and is less than 6 and not 0" .format(number, num))
 else :
     print("last digit of {} is {} and is zero" .format(number, num))
--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def pow(a, b):
-    powerUp= a**b
-    return powerUp
+    p= a**b
+    return p
