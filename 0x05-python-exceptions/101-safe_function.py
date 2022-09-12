de032438a20251504c1bdd865d6a2b7101d78556
arisa-e <emmanuelarisa@outlook.com>
2022-09-12 21:58:43 +0300

    initial commit

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_function(fct, *args):
+
     try:
         return fct(*args)
-    except (ZeroDivisionError, ValueError, TypeError, IndexError) as error:
-        print("Exception: {}".format(error), file=sys.stderr)
-        return None
+    except Exception as exception:
+        print('Exception: {}'.format(exception), file=sys.stderr)
